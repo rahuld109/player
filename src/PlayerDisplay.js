67c798b3f7312ac79ac89dccd2a8b750a46fa633
Rahul Dogra <rahuldogra1998@gmail.com>
2021-02-24 20:42:39 +0530

    match data added

--- src/PlayerDisplay.js
+++ src/PlayerDisplay.js
@@ -9,21 +9,43 @@ function PlayerDisplay(props) {
       console.log(player);
       return (
         <div className="player__container" key={player.Id}>
-          <img
-            className="player__image"
-            key={player.Id}
-            src={`./images/${player.Id}.jpg`}
-            alt="Unavailable"
-          />
+          <div>
+            <img
+              className="player__image"
+              key={player.Id}
+              src={`./images/${player.Id}.jpg`}
+              alt="Unavailable"
+            />
+          </div>
           <div className="player__name">
             <h3>{player.PFName}</h3>
             <h3 className="skill__badge">{player.SkillDesc}</h3>
           </div>
+          <div className="player__info">
+            <h3>💲{player.Value} Million </h3>
+          </div>
+          {player.UpComingMatchesList.map((match, i) => {
+            return (
+              <div className="match__container">
+                <div className="player__info" align="center" key={i}>
+                  <h3>
+                    {match.CCode ? match.CCode : "NA"} vs.{" "}
+                    {match.VsCCode ? match.VsCCode : "NA"}
+                  </h3>
+                </div>
+                <div className="upcoming__matches" align="center">
+                  <div className="match__date">
+                    {match.MDate ? match.MDate : "NA"}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       );
     });
   } else {
-    return <h3>No Player Data yet</h3>;
+    return <h3>No Player Data Has Been Feteched Yet</h3>;
   }
 }
 
